97fd76f976ad5ed9c98bf1b6ed39d590a018a7bd
Dikay900 <dark900@xyz.de>
2020-04-18 12:08:38 +0200

    remove common vendor

--- sm6125-common.mk
+++ sm6125-common.mk
@@ -11,7 +11,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Get non-open-source specific aspects
-$(call inherit-product-if-exists, vendor/xiaomi/sm6125-common/sm6125-common-vendor.mk)
+#$(call inherit-product-if-exists, vendor/xiaomi/sm6125-common/sm6125-common-vendor.mk)
 
 # Vendor properties
 -include $(LOCAL_PATH)/vendor_prop.mk
