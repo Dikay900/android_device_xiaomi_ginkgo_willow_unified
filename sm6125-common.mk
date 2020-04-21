f57cac8424f248e0c0f298c2e9f13cca56da77f3
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-04-21 16:54:40 +0530

    ginkgo: Update wifi/wigig blobs from laurel_sprout
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

--- sm6125-common.mk
+++ sm6125-common.mk
@@ -442,7 +442,7 @@ PRODUCT_PACKAGES += \
     wpa_supplicant \
     wpa_supplicant.conf \
     vendor.qti.hardware.wifi.hostapd@1.1.vendor \
-    vendor.qti.hardware.wifi.supplicant@2.1.vendor
+    vendor.qti.hardware.wifi.supplicant@2.0.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
