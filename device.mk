86a8c7f6e3ee6e98034fba901cd60b56ff36394a
Dikay900 <dark900@xyz.de>
2020-07-30 20:32:24 +0000

    use aeonax anxcamera

--- device.mk
+++ device.mk
@@ -82,6 +82,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
 
+# ANXCamera
+$(call inherit-product-if-exists, vendor/aeonax/ANXCamera/anx-vendor.mk)
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio.effect@5.0-impl \
